b8e15cbcfc1a308f8cb0b3eef5f695ba0a151906
Federico Ulfo <fulfo@sailthru.com>
2013-02-05 15:31:47 -0500

    Added unit test

---
+++ tests/UserAgentTest.php
@@ -0,0 +1,17 @@
+<?php
+
+class Sail_UserAgentTest extends PHPUnit_Framework_TestCase {
+
+    public function setup(){
+        
+    }
+    
+    /**
+     * 
+     */
+    public function testUserAgents(){
+        $this->assertTrue( 1 == 1 );
+    }
+    
+    
+}
